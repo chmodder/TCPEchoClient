84f95ae3e0845362d72184b79fe97000fec6eed8
chmodder <hotmailgateway@gmail.com>
2017-02-19 19:08:36 +0100

    Add project files.

---
+++ TCPEchoClient/TCPEchoClient.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * TCPEchoClient
+ *
+ * Author Michael Claudius, ZIBAT Computer Scienc
+ * Version 1.0. 2014.02.10
+ * Copyright 2014 by Michael Claudius
+ * Revised 2014.09.01
+ * All rights reserved
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPEchoClient
+{
+    class TCPEchoClient
+    {
+        static void Main(string[] args)
+        {
+            //Console.ReadLine();
+            TcpClient clientSocket = new TcpClient("192.168.3.112", 6789);
+
+            Stream ns = clientSocket.GetStream();  //provides a Stream
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true; // enable automatic flushing
+
+            string message = Console.ReadLine();
+            sw.WriteLine(message);
+            string serverAnswer = sr.ReadLine();
+            Console.WriteLine("Server: " + serverAnswer);
+
+            ns.Close();
+
+            clientSocket.Close();
+            Console.ReadKey();
+
+        }
+
+    }
+
+
+
+
+}
